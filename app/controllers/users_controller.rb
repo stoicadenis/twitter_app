5f42be7e0232a4f64b796e78e1d07102899a9d90
Stoica Denis <stoicadenis90@yahoo.com>
2022-04-15 18:05:53 +0300

    Finished lab 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,19 @@ class UsersController < ApplicationController
   	def show
   		@user = User.where(id: params[:id]).first
   	end
+
+  	def new
+  		@user = User.new
+  	end
+
+  	def create
+  		user = User.new(name: params['user']['name'])
+
+  		if user.save
+  			redirect_to "/users/#{user.id}"
+  		else
+  			flash[:error] = 'Validations failed'
+  			redirect_to '/users/new'
+  		end
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get 'other_route', to: 'application#other_route'
   get 'create_user', to: 'application#create_user'
   get 'users', to:'users#index'
+  get 'users/new', to:'users#new'
   get 'users/:id', to:'users#show'
+  post 'users', to: 'users#create'
 end
